be5ec27f6d5e59bf2183d1bcd19188cda8413319
Louis Amon <contact@louisamon.com>
2018-03-20 10:02:29 +0000

    Statically linking BLAS/LAPACK binaries with numpy

--- lambda_packages/numpy/build.sh
+++ lambda_packages/numpy/build.sh
@@ -1,13 +1,17 @@
 BUILD_DIR=build
-# NB: numpy has to be build along with pandas for proper linkage between libraries
 pip-3.6 install --target $BUILD_DIR --no-binary numpy numpy
 cd $BUILD_DIR
+# Copy BLAS/LAPACK binaries
+mkdir -p lib
+cp /usr/lib64/atlas/* lib
+cp /usr/lib64/libquadmath.so.0 lib
+cp /usr/lib64/libgfortran.so.3 lib
 # Compress binaries
-du -sh numpy
-find numpy -name "*.so"|xargs strip
-du -sh numpy
+du -sh *
+find . -name "*.so"|xargs strip
+du -sh *
 # Create archive
-tar -czvf ../python3.6-numpy-1.14.2.tar.gz numpy
+tar -czf ../python3.6-numpy-1.14.2.tar.gz numpy
 # Check version
 python3 -c "import numpy;print(numpy.__version__)"
 # Remove artifacts
